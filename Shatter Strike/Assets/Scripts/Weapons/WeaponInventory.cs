38868bf1089de0e98b09dac66cf663433b47c5fc
Egor-04 <egor04m@gmail.com>
2022-10-31 19:41:01 +0300

    Added new Weapon TT 33

--- Shatter Strike/Assets/Scripts/Weapons/WeaponInventory.cs
+++ Shatter Strike/Assets/Scripts/Weapons/WeaponInventory.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using System;
+using DitzelGames.FastIK;
 
 public class WeaponInventory : MonoBehaviour
 {
     [SerializeField] private float _mouseScrollValue;
+    [SerializeField] private FastIKFabric _leftHand, _rightHand;
     [SerializeField] private WeaponItem[] _weaponsBar;
     private KeyCode[] _inputs = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4 };
     [SerializeField] private int _j = 0;
@@ -14,6 +16,7 @@ public class WeaponInventory : MonoBehaviour
     {
         public DropedWeapon DropedWeapon;
         public Weapon HandWeapon;
+        public Transform LeftHandTargetIK, RightHandTargetIK;
         public int CurrentType = 0;
         public bool CanChangeOther = false;
         public bool HasItem = false;
@@ -33,7 +36,7 @@ public class WeaponInventory : MonoBehaviour
             {
                 for (int j = 0; j < _weaponsBar.Length; j++)
                 {
-                    if(_weaponsBar[j].CanChangeOther)
+                    if (_weaponsBar[j].CanChangeOther)
                     {
                         if (_weaponsBar[j].CurrentType == (i+1))
                         {
@@ -42,6 +45,8 @@ public class WeaponInventory : MonoBehaviour
                             sameTypeCount++;
 
                             _weaponsBar[_j].HandWeapon.gameObject.SetActive(true);
+                            _leftHand.Target = _weaponsBar[_j].LeftHandTargetIK;
+                            _rightHand.Target = _weaponsBar[_j].RightHandTargetIK;
 
                             if (_j < sameTypeCount)
                             {
@@ -51,15 +56,16 @@ public class WeaponInventory : MonoBehaviour
                             {
                                 _j = 0;
                             }
-                            return;
                         }
                     }
                     else
                     {
                         DeactivateAll();
-                        if ((i+1) == _weaponsBar[j].CurrentType)
+                        if ((i + 1) == _weaponsBar[j].CurrentType)
                         {
                             _weaponsBar[j].HandWeapon.gameObject.SetActive(true);
+                            _leftHand.Target = _weaponsBar[j].LeftHandTargetIK;
+                            _rightHand.Target = _weaponsBar[j].RightHandTargetIK;
                             return;
                         }
                     }
